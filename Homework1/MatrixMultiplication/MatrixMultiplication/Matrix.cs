5b937405efec9639a1c11ae086425df3d87957ae
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-09-10 16:03:54 +0300

    feat: create method to generate matrix

--- Homework1/MatrixMultiplication/MatrixMultiplication/Matrix.cs
+++ Homework1/MatrixMultiplication/MatrixMultiplication/Matrix.cs
@@ -2,24 +2,41 @@
 
 public class Matrix
 {
-    public int[,] MatrixData { get; private set; }
-    public int Rows { get; private set; }
-    public int Columns { get; private set; }
-
-    public Matrix(int[,] data)
+    private static readonly Random rand = new Random();
+    public int[,] MatrixData { get; }
+    public int Rows { get; }
+    public int Columns { get; }
+    
+    public Matrix(int[,] data) 
     {
         MatrixData = data;
         Rows = data.GetLength(0);
         Columns = data.GetLength(1);
     }
 
+    
+    public static Matrix GenerateMatrix(int rows, int columns)
+    {
+        var newMatrixData = new int[rows, columns];
+        for (var i = 0; i < rows; ++i)
+        {
+            for (var j = 0; j < columns; ++j)
+            {
+                newMatrixData[i, j] = rand.Next(-100, 101);
+            }
+        }
+
+        return new Matrix(newMatrixData);
+    }
+
+    
     public Matrix Multiply(Matrix secondMatrix)
     {
         if (!MultiplicationMatching(secondMatrix))
         {
             throw new ArgumentException();
         }
-
+        
         var newMatrixData = new int[Rows, secondMatrix.Columns];
         for (var i = 0; i < Rows; ++i)
         {
@@ -32,17 +49,65 @@ public class Matrix
             }
         }
 
-        var newMatrix = new Matrix(newMatrixData);
-        return newMatrix;
+        return new Matrix(newMatrixData);
     }
 
-    bool MultiplicationMatching(Matrix secondMatrix)
+
+    public Matrix ParallelMultiply(Matrix secondMatrix)
     {
-        if (Columns != secondMatrix.Rows)
+        if (!MultiplicationMatching(secondMatrix))
         {
-            return false;
+            throw new ArgumentException();
         }
 
-        return true;
+        var newMatrixItemsCount = Rows * secondMatrix.Columns;
+        var newMatrixData = new int[Rows, secondMatrix.Columns];
+
+        var threadsCount = Environment.ProcessorCount;
+        var threads = new Thread[threadsCount];
+
+        var itemsForThread = (newMatrixItemsCount) / threadsCount;
+        var remainder = (newMatrixItemsCount) % threadsCount;
+
+        for (var threadNumber = 0; threadNumber < threadsCount; ++threadNumber)
+        {
+            var nestedThreadNumber = threadNumber;
+            threads[threadNumber] = new Thread(() =>
+            {
+                var startedItemNumber = nestedThreadNumber * itemsForThread + Math.Min(nestedThreadNumber, remainder);
+                var lastItemNumber = startedItemNumber + itemsForThread;
+
+                if (nestedThreadNumber < remainder)
+                {
+                    lastItemNumber++;
+                }
+
+                for (var itemNumber = startedItemNumber; itemNumber < lastItemNumber; ++itemNumber)
+                {
+                    var row = itemNumber / Rows;
+                    var column = itemNumber % secondMatrix.Columns;
+
+                    for (var i = 0; i < Columns; ++i)
+                    {
+                        newMatrixData[row, column] += MatrixData[row, i] * secondMatrix.MatrixData[i, column];
+                    }
+                }
+            });
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        return new Matrix(newMatrixData);
     }
+
+    
+    private bool MultiplicationMatching(Matrix secondMatrix) => Columns == secondMatrix.Rows;
 }
--- Homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
+++ Homework1/MatrixMultiplication/MatrixMultiplication/Program.cs
@@ -1,17 +1,29 @@
 ﻿using MatrixMultiplication;
 
 
-var firstMatrix = new Matrix(new int[2, 2] { { 1, 1 }, { 2, 2 } });
-var secondMatrix = new Matrix(new int[2, 2] { { 3, 3 }, { 4, 4 } });
+var firstMatrix = Matrix.GenerateMatrix(10, 10);
+var secondMatrix = Matrix.GenerateMatrix(10, 10);
 
-var resultMatrixData = firstMatrix.Multiply(secondMatrix).MatrixData;
+var resultMatrixData1 = firstMatrix.Multiply(secondMatrix).MatrixData;
+var resultMatrixData2 = firstMatrix.ParallelMultiply(secondMatrix).MatrixData;
 
 
-for (var i = 0; i < resultMatrixData.GetLength(0); ++i)
+
+for (var i = 0; i < resultMatrixData1.GetLength(0); ++i)
 {
     Console.WriteLine();
-    for (var j = 0; j < resultMatrixData.GetLength(1); ++j)
+    for (var j = 0; j < resultMatrixData1.GetLength(1); ++j)
     {
-        Console.Write(resultMatrixData[i, j] + " ");
+        Console.Write(resultMatrixData1[i, j] + " ");
+    }
+}
+
+
+for (var i = 0; i < resultMatrixData2.GetLength(0); ++i)
+{
+    Console.WriteLine();
+    for (var j = 0; j < resultMatrixData2.GetLength(1); ++j)
+    {
+        Console.Write(resultMatrixData2[i, j] + " ");
     }
 }
