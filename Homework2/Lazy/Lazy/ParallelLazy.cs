aa8079c030fe64041765baea035b1c0ea5e65ffc
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-09-16 02:43:52 +0300

    feat: Create ParallelLazy implementation

---
+++ Homework2/Lazy/Lazy/ParallelLazy.cs
@@ -0,0 +1,50 @@
+﻿namespace Lazy;
+
+public class ParallelLazy<T>
+{
+    private  Func<T>? _supplier;
+    private  T? _value;
+    
+    private volatile LazyState _state;
+
+    public ParallelLazy(Func<T> supplier)
+    {
+        _supplier = supplier;
+        _state = LazyState.NotReceived;
+    }
+
+
+    public T? Get()
+    {
+        // Reading _state updates _value and _supplier from memory!
+        // This condition must be first!
+        if (_state != LazyState.NotReceived)
+        {
+            return _value;
+        }
+
+        if (_supplier == null)
+        {
+            throw new InvalidOperationException("Null supplier is not allowed.");
+        }
+        
+        lock (_supplier)
+        {
+            // Reading _state updates _value and _supplier from memory!
+            // This condition must be first!
+            if (_state == LazyState.ReceivedBySupplier)
+            {
+                return _value;
+            }
+            
+            // Writing _state updates _value and _supplier in memory!
+            // Writing _state should be the last statement!
+            _value = _supplier();
+            _supplier = null;
+            
+            _state = LazyState.ReceivedBySupplier;
+            
+            return _value;
+        }
+    }
+}
