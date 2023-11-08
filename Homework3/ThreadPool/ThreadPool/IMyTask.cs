350479867207f3bb77283b8c2661b7f531298d61
BakhorikovEgor <117931736+BakhorikovEgor@users.noreply.github.com>
2023-11-08 21:23:10 +0300

    fix: add access modifiers

--- Homework3/ThreadPool/ThreadPool/IMyTask.cs
+++ Homework3/ThreadPool/ThreadPool/IMyTask.cs
@@ -9,14 +9,14 @@ public interface IMyTask<out TResult>
     /// <summary>
     /// Gets a value indicating whether the task has completed.
     /// </summary>
-    bool IsCompleted { get; }
+    public bool IsCompleted { get; }
     
     /// <summary>
     /// Gets the result of the task.
     /// Blocks the thread waiting for the result to be calculated if it has not already occurred.
     /// </summary>
     /// <exception cref="AggregateException"> Error occurred during task calculation. </exception>
-    TResult? Result { get; }
+    public TResult? Result { get; }
 
     /// <summary>
     /// Creates a new task that will be executed after the completion of the current task.
@@ -25,5 +25,5 @@ public interface IMyTask<out TResult>
     /// <param name="func">The delegate that will transform the result of the current task into the result of the new task.</param>
     /// <returns>A new task of type IMyTask </returns>
     /// <exception cref="InvalidOperationException"> New task can not be created. </exception>
-    IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> func);
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult?, TNewResult> func);
 }
